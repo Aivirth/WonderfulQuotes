b87919f1ac719990af8aaba10de7c118f4613b66
Aivirth <aivirth.work@gmail.com>
2018-03-22 22:55:33 +0100

    Added New Quotes Component basic structure

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,17 +1,19 @@
 <template>
-<div class="col-sm-6 col-md-4 col-lg-3">
-  <div class="panel panel- default">
-    <div class="panel-body quote">
-      <slot></slot>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="panel panel- default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
     </div>
   </div>
-</div>
 </template>
+
 <script>
 export default {
   props: ["quotes"]
 };
 </script>
+
 <style>
 .panel-body {
   font-family: "Arizonia", cursive;
