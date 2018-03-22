c09fae815f00a1f993fd70c4618863ba18e22cd3
Aivirth <aivirth.work@gmail.com>
2018-03-22 22:44:20 +0100

    Added Grid and Quote Components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
     <div class="container">
-    
+    <quote-grid :quotes="quotes"></quote-grid>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import QuoteGrid from "./components/QuoteGrid.vue";
+export default {
+  data() {
+    return {
+      quotes: ["quote placeholder"],
+      maxQuotes: 10
+    };
+  },
+  components: {
+    quoteGrid: QuoteGrid
+  }
+};
 </script>
 
 <style>
+
 </style>
